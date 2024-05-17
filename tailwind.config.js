6782edc623df81f25c188c86239d1897f5c118f6
liettexx <enkhoyuka469@gmail.com>
2024-05-17 15:35:49 -0400

    twelfth

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        cornflowerblue: "#6495ed",
+        grayish: "#96969621",
+        bluish: "#0095f6",
+        graytone: "#ffffff24",
+        darkgray: "#FFFFFF36",
+        slightgray: "#FFFFFF6C",
+        loadcolor: "#0000001a",
+        oceanblue: "#3498db",
+      },
+    },
+  },
+  plugins: [],
+};
